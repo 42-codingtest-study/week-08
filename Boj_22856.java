4287a3ebb36f0b0178923bc3950b184d8c1ab907
miffyKing <whqudghk28@gmail.com>
2023-01-07 20:00:36 +0900

    01/07 second commit!!

--- Boj_22856.java
+++ Boj_22856.java
@@ -24,18 +24,15 @@ public class Boj_22856 {
             table.get(curNode).add(lChild);
             table.get(curNode).add(rChild);
         }
-        inorder(1);        //3) 중위 순회
+        inorder(1);        //3) 중위 순회. 중위순회 과정이 inorderResult에 저장됩니다.
         int inorderLastVisit = inorderResult.get(inorderResult.size()-1);       //4) 중위 순회의 마지막 방문 값
 
         //이제 dfs 를 통해 루트 노드에서 중위순회 마지막 값 까지의 경로 길이를 구한다.
         int totalVisits = doDFS(1, inorderLastVisit, n);
 
-        //방법1.
-        //int toLastVisit = visited[inorderLastVisit];      //가장 마지막 방문 노드가 뭔지 저장한다.
-
-        //방법2.
         int toLastVisit = 0;
-        int currentNode= 1;
+
+        int currentNode= 1;         // 1부터 시작해서 오른쪽 가장 아래 노드까지 향하는 거리를 계산합니당.
         while (true) {
             int next = table.get(currentNode).get(1);
             if (next != -1) {
@@ -44,12 +41,10 @@ public class Boj_22856 {
             }
             else break;
         }
-        //방법1이 틀리고 방법2가 된 이유는, 총 간선의 길이 2배에서 제외해 주어야 하는 값이 "루트에서 가장 마지막 중위순회 방문 노드 까지의 길이" 가 아니라
-        // "루트에서 오른쪽으로 가장 멀리 떨어져 있는 노드 까지의 길이" 이기 때문이다.
-        // 반례로는 1번 노드에 2번 노드가 왼쪽 자식으로 달려있는 경우가 있겠다.
-            // 이 케이스에서 내 방법1의 경우에는 값이 1이 나오는데, 간선의 길이(1) * 2 - 1 (가장 마지막 방문 노드(2) 까지의 길이) 이기 때문이다.
-            // 근데 이 경우는 1 -> 2 -> 1 로 방문을 하고 종료이기 때문에 답이 2가 나와야 하는것임.
+
         System.out.println(totalVisits*2 - toLastVisit);
+        //가장 마지막 노드 까지 dfs로 이동한 길이의 2배에서  루트노드에서 맨 마지막 노드이면서 오른쪽에 있는 노드일 경우까지 이동한 경우만큼 빼주면 된다...
+        //다시 돌아오는 과정이 없으니까!!
     }
 
     public static void inorder(int cur) {
@@ -84,4 +79,4 @@ public class Boj_22856 {
 }
 
 
-//보고했어염 https://viyoung.tistory.com/346
+//이게 도와줌. https://viyoung.tistory.com/346
