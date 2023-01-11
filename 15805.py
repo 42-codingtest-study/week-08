4c30bc1bbfdad90c992779d3df445f006aeced3c
Chanwoong1 <jcw7468@gmail.com>
2023-01-11 14:23:40 +0900

    feat: 15805 드디어 성공

--- 15805.py
+++ 15805.py
@@ -4,8 +4,8 @@
 K = int(input())
 map_lst = list(map(int, input().split()))
 tree = set()
-visited = [0] * (K)
-visited[0] = -1
+visited = [0] * K
+visited[map_lst[0]] = -1
 tree.add(map_lst[0])
 for i in range(len(map_lst)) :
     if map_lst[i] not in tree :
